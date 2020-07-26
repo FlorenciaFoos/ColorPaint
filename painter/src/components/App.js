76c794c53fe72bb0d8d4dea8872fbd1c07cd7e72
FlorenciaFoos <florenciamfoos@gmail.com>
2020-07-26 20:46:24 -0300

    first commit

---
+++ painter/src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import ColorPicker from "./ColorPicker";
+import PaintGrid from "./PaintGrid";
+import ColorPreview from "./ColorPreview";
+import { colors, PainterContext } from "../contexts/PainterContext";
+
+function App() {
+  // use state for color and setColor
+  const [color, setColor] = React.useState(colors[0]);
+
+  return (
+    <div className="App container">
+      <PainterContext.Provider value={{colors, color, setColor}}>
+        <h1 className='title'>Let´s paint!</h1>
+        <ColorPicker />
+        <ColorPreview />
+        <br />
+        <PaintGrid />
+      </PainterContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ painter/src/contexts/PainterContext.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// define list of colors
+export const colors = [
+  { name: "white", hex: "#fff" },
+  { name: "dark purple", hex: "#270036" },
+  { name: "dark fuchsia", hex: "#5E003C" },
+  { name: "red", hex: "#960000" },
+  { name: "orange", hex: "#C92600" },
+  { name: "blue", hex: "#0A3A4A" },
+  { name: "yellow", hex: "#FFCE00" },
+  { name: "green", hex: "#9AC836" },
+  { name: "light blue", hex: "#33A6B2" },
+  { name: "light purple", hex: "#9370db" }
+];
+
+// create painter context
+// make the first color its default value
+export const PainterContext = React.createContext();
